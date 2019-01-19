6dae5a72433759797f7d7a708bf1d7d8c164266c
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-01-18 23:32:55 -0500

    Fixed API errors

--- togglbot.py
+++ togglbot.py
@@ -12,10 +12,11 @@ import json
 import sys
 import re
 import os
-import platform
 import random
 import subprocess
 
+from time import sleep
+from dateutil.tz import tzoffset
 from urlparse import urlparse
 from six.moves.urllib.parse import urlparse
 from random import randint
@@ -29,64 +30,67 @@ ustring = "jandrews7348@floridapoly.edu:PASSWORD"
 username = "email@floridapoly.edu"
 password = "Password123"
 
+now = datetime.datetime.now()
+time = now.hour
+print now.hour, now.minute
+
+while now.hour > 7:
 # SEED
-seed = randint(11111,99999)
-slotv1 = int(randint(1200,3600))
-slotv2 = randint(1200,3600)
-slotv3 = randint(1200,3600)
-slotv4 = randint(1200,3600)
-slotv5 = randint(1200,3600)
-slotv6 = randint(1200,3600)
-slotv7 = randint(1200,3600)
-slotv8 = randint(1200,3600)
-
-date = datetime.datetime.now()
-#Add more activities to add more entropy and make it more interesting! |
-
-hw = ["Calculus", "Break", "Study", "Physics", "LAB", "Misc.", "Robotics", "Programming", "Study group", "Project", "C&M"]
-#LOG DATA (before sending)
-
-
-print slotv1 / 60, "Minutes", random.choice(hw)
-print slotv2 / 60, "Minutes", random.choice(hw)
-print slotv3 / 60, "Minutes", random.choice(hw)
-print slotv4 / 60, "Minutes", random.choice(hw)
-print slotv5 / 60, "Minutes", random.choice(hw)
-print slotv6 / 60, "Minutes", random.choice(hw)
-print slotv7 / 60, "Minutes", random.choice(hw)
-print slotv8 / 60, "Minutes", random.choice(hw)
-
-v1 = slotv1, "Minutes", random.choice(hw)
-v2 = slotv2, "Minutes", random.choice(hw)
-v3 = slotv3, "Minutes", random.choice(hw)
-v4 = slotv4, "Minutes", random.choice(hw)
-v5 = slotv5, "Minutes", random.choice(hw)
-v6 = slotv6, "Minutes", random.choice(hw)
-v7 = slotv7, "Minutes", random.choice(hw)
-v8 = slotv8, "Minutes", random.choice(hw)
-
-hw1 = str(random.choice(hw))
-hw2 = random.choice(hw)
-hw3 = random.choice(hw)
-hw4 = random.choice(hw)
-hw5 = random.choice(hw)
-hw6 = random.choice(hw)
-hw7 = random.choice(hw)
-hw8 = random.choice(hw)
-
-# Get API Token
-wid = 3164178
-os.system('echo running shell...')
-APIT = str(os.popen('curl -s -u %s -X POST https://www.toggl.com/api/v8/reset_token | sed \'s/"//g\'' % ustring).read())
-print "Token:", APIT
-
-# Make Project
-
-sdate = str(os.popen('date --rfc-3339=seconds | sed \'s/ /T/g\'').read())
-
+	slotv1 = int(randint(1200,3600))
+	slotv2 = randint(1200,3600)
+	slotv3 = randint(1200,3600)
+	slotv4 = randint(1200,3600)
+	slotv5 = randint(1200,3600)
+	slotv6 = randint(1200,3600)
+	slotv7 = randint(1200,3600)
+	slotv8 = randint(1200,3600)
+	
+	#Add more activities to add more entropy and make it more interesting! |
+	
+	hw = ["Calculus", "Break", "Study", "Physics", "LAB", "Misc.", "Robotics", "Programming", "Study group", "Project", "C&M"]
+	#LOG DATA (before sending)
+	
+	hw1 = random.choice(hw)
+	hw2 = random.choice(hw)
+	hw3 = random.choice(hw)
+	hw4 = random.choice(hw)
+	hw5 = random.choice(hw)
+	hw6 = random.choice(hw)
+	hw7 = random.choice(hw)
+	hw8 = random.choice(hw)
+	
+	# Get API Token
+	wid = 3164178
+	APIT = str(os.popen('curl -s -u %s -X POST https://www.toggl.com/api/v8/reset_token | sed \'s/"//g\'' % ustring).read())
+	
+	# Test Token
+	if len(APIT) > 1:
+		print "--> Connected"
+		print "Token:", APIT
+		
+	
 # Build string
-#scommand = ("curl -v -u %s:api_token -H \"Content-Type: application/json\"\'{\"time_entry\":{\"description\":\"%s\",\"created_with\":\"togglbot\",\"start\":\"%s\",\"duration\":%i,\"wid\":3164178}}\' -X POST https://www.toggl.com/api/v8/time_entries" % APIT, hw1, sdate, slotv1)
-#os.system(scommand)
+	
+# Date
+	now = datetime.datetime.now(tzoffset('EDT', +4*60*60)).replace(microsecond=0)
+	sdate = now.isoformat()
+	# JSON Data
+	time_entry1 = {'time_entry':{
+	                "description": hw1,
+	                "start": sdate,
+	                "duration": slotv1,
+	                "created_with": "togglbot"
+	}}
+# Check JSON
+	print "--> Request Sent:", time_entry1
+	
+	# Shell request 2.0 (cURL)
+	# os.system('curl -u '+str(os.popen('curl -s -u %s -X POST https://www.toggl.com/api/v8/reset_token | sed \'s/\"//g\'' % ustring).read())+':api_token -H \"Content-Type: application/json\" -d \'{\"time_entry\":{\"description\":\"'+str(random.choice(hw))+'\",\"created_with\":\"togglbot\",\"start\":'+str(os.popen('date --rfc-3339=seconds | sed \'s/ /T/g\'').read())+',duration:'+str(int(randint(1200,3600)))+',\"wid\":3164178}}\' -X POST https://www.toggl.com/api/v8/time_entries')
+	
+# Python Request 3.0
+	request1 = requests.post('https://www.toggl.com/api/v8/time_entries', auth=(APIT, 'api_token'), data=json.dumps(time_entry1))
+	if not request1: # request.Response returns True if status 'OK'
+	print(request1.text)
+	sleep(3600)
+	now = datetime.datetime.now()
 
-# Build request 2.0
-os.system('curl -u '+str(os.popen('curl -s -u %s -X POST https://www.toggl.com/api/v8/reset_token | sed \'s/"//g\'' % ustring).read())+':api_token -H \"Content-Type: application/json\"\'{\"time_entry\":{\"description\":'+str(random.choice(hw))+',\"created_with\":\"togglbot\",\"start\":'+str(os.popen('date --rfc-3339=seconds | sed \'s/ /T/g\"').read())+',duration:'+str(int(randint(1200,3600)))+',wid\":3164178}}\' -X POST https://www.toggl.com/api/v8/time_entries')
